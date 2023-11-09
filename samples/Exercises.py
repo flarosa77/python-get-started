f0e65c8dd0bfbd82a2095357b642c9e8b58cc14f
flarosa77 <flarosa@gmail.com>
2023-11-09 20:21:45 +0100

    Update Exercises.py

--- samples/Exercises.py
+++ samples/Exercises.py
@@ -1,11 +1,11 @@
 """
-1. Scrivere un codice che chieda in input all'utente un range di numeri (inizio e fine) e poi stampi a video solo i numeri pari
-2. Modificare il codice al punto 1. per scrivere pari: e il numero se quest'ultimo è pari, altrimenti scrivere dispari: e il numero
-3. Modificare il codice al punto 2. per scrivere solo i numeri pari multipli di 3 e i numeri dispari multipli di 5
+1. Scrivere un codice che chieda in input all'utente un range di numeri (inizio e fine) e poi stampi a video solo i numeri pari compresi tra inizio e fine.
+2. Modificare il codice al punto 1 in modo da visualizzare solo i numeri pari compresi tra inizio e fine se quest'ultimo è pari, altrimenti visualizzare i numeri dispari.
+3. Modificare il codice al punto 2 in modo da visualizzare solo i numeri pari multipli di 3 e i numeri dispari multipli di 5 compresi tra inizio e fine.
 """
 
 '''
-1. Create una variabile che contenga la scritta "Ronaldo,Zlatan,Lukaku" e e chiamate il metodo split() passando l'argomento ,
+1. Create una variabile che contenga la scritta "Ronaldo,Zlatan,Lukaku" e chiamate il metodo split() passando l'argomento ,
 '''
 
 
@@ -28,8 +28,8 @@ tupla_test = (11,)
 '''
 
 '''
-1. Dati i due set:
-svolgete le quattro operazioni tra set utilizzando i metodi union() , intersection() , difference() e symmetric_difference()
+1. Dati due set:
+svolgete quattro operazioni tra set utilizzando i metodi union() , intersection() , difference() e symmetric_difference()
 '''
 
 '''
@@ -46,7 +46,7 @@ e lavoriamoci un po' su.
 1. Contate le righe (di effettivo testo) che compongono l'estratto
 2. Contate le parole che compongono l'estratto
 3. Scrivete al contrario il terzo verso
-4. Create un dizionario che mappi ogni carattere (chiave) con la sua occorrenza nel
+4. Create un dizionario che mappi ogni carattere (chiave) con la sua occorrenza/posizione nel
 testo (valore)
 5. Create un dizionario come il precedente per le sole lettere (no caratteri speciali),
 ignorando maiuscole e minuscole
@@ -61,7 +61,7 @@ ignorando maiuscole e minuscole
 1. Leggete il file divina_commedia_extended.txt e scrivete un programma per contare:
 il numero di versi nel file
 il numero di parole nel file
-il numero di caratteri diversi nel file
+il numero di caratteri nel file
 
 2. Leggete il file rubrica.txt , stampatene il contenuto e create un dizionario che
 abbia come chiavi i nomi delle colonne e come valori associati la lista dei valori della colonna
