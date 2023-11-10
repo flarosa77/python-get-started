9e333aa16983d33ccf7fa434bc94e9c206005684
flarosa77 <flarosa@gmail.com>
2023-11-10 20:22:41 +0100

    Update Functions.py

--- samples/Functions.py
+++ samples/Functions.py
@@ -1,4 +1,4 @@
-def moreTimes():
+def moreTimes(): # usa def per creare una funzione
   for i in range(0,50):
     print("Hello World!",'\n')
 
@@ -17,18 +17,45 @@ num = int(input('How many times you want repeat?\n'))
 moreTimes(num)
 
 
-def somma(a, b):  # usa def per creare una funzione
+'''Introduciamo una nuova funzione in grado di concatenare due stringhe '''
+
+def concat(str1,str2):
+ return str1 + ' ' + str2
+
+str = concat('Marco','Rossi')
+
+print(str)
+
+'''Apportiamo una piccola modifica alla funzione precedente'''
+
+def upAndConcat(str1,str2):
+  return str1.upper() + ' ' + str2.upper()
+
+str = upAndConcat('Marco','Rossi')
+
+print(str)
+
+'''Modifichiamo la funzione precedente introducendo la f-string'''
+def upAndConcat2(str1,str2):
+  return f"{str1.upper()} {str2.upper()}"
+  
+str = upAndConcat2('Marco','Rossi')
+
+print(str)
+
+
+def sum(a, b):  
     return a + b
 
 
-def fattoriale(a):
+def factorial(a):
     result = 1
     for i in range(1, a + 1):
         result *= i
     return result
 
 
-def stampa(msg):
+def newprint(msg):
     print(msg)
     print()
 
@@ -40,17 +67,17 @@ def moreOps(a, b):
 
 
 
-c = somma(10, 30)
+c = sum(10, 30)
 
 print(c)
 
-d = fattoriale(7)
+d = factorial(7)
 
 print(d)
 
-stampa("Hello world!")
+newprint("Hello world!")
 
-tuple = (sum, diff) = moreOps(10, 5)
+tuple = (add, diff) = moreOps(10, 5)
 
-print(sum,'\n')
+print(add,'\n')
 print(diff,'\n')
